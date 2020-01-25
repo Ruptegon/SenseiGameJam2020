783041735489004f79d0c9aeecd9ada24dad7636
rivFox <rivfox.jb@gmail.com>
2020-01-25 04:21:36 +0100

    Add Player default movement (?)

--- Assets/Scripts/ClientUIController.cs
+++ Assets/Scripts/ClientUIController.cs
@@ -12,17 +12,17 @@ public class ClientUIController : MonoBehaviour
     [SerializeField] private Button RightArrow; 
     [SerializeField] private Button LeftArrow;
 
-    private float horizontalInput;
-    private float vertiaclInput;
+    public static float HorizontalInput;
+    public static float VertiaclInput;
 
     private void Awake()
     {
         gameObject.SetActive(Net.IsClient);
 
-        UpArrow.onClick.AddListener(() => IncreaseValue(ref vertiaclInput));
-        DownArrow.onClick.AddListener(() => DecreaseValue(ref vertiaclInput));
-        RightArrow.onClick.AddListener(() => IncreaseValue(ref horizontalInput));
-        LeftArrow.onClick.AddListener(() => DecreaseValue(ref horizontalInput));
+        UpArrow.onClick.AddListener(() => IncreaseValue(ref VertiaclInput));
+        DownArrow.onClick.AddListener(() => DecreaseValue(ref VertiaclInput));
+        RightArrow.onClick.AddListener(() => IncreaseValue(ref HorizontalInput));
+        LeftArrow.onClick.AddListener(() => DecreaseValue(ref HorizontalInput));
     }
 
     private void IncreaseValue(ref float value)
@@ -37,6 +37,6 @@ public class ClientUIController : MonoBehaviour
 
     private void Update()
     {
-        Debug.Log($"Horizontal: {horizontalInput}, Vertical: {vertiaclInput}");
+        Debug.Log($"Horizontal: {HorizontalInput}, Vertical: {VertiaclInput}");
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    private CharacterController characterController;
+
+    [Header("Movement Settings")]
+    public float moveSpeed = 8f;
+    public float turnSensitivity = 5f;
+    public float maxTurnSpeed = 150f;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!isLocalPlayer || characterController == null) return;
+
+        var horizontal = ClientUIController.HorizontalInput;
+        var vertical = ClientUIController.VertiaclInput;
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        direction = Vector3.ClampMagnitude(direction, 1f);
+        direction = transform.TransformDirection(direction);
+        direction *= moveSpeed;
+
+        characterController.SimpleMove(direction);
+    }
+}
