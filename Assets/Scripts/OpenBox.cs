a3bbf3d5fa0699cb858b1904e852fcba214ca38b
Derko666 <malowic@gmail.com>
2020-01-25 15:41:37 +0100

    Added new objects and updated mimic prefab

---
+++ Assets/Scripts/OpenBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBox : MonoBehaviour
+{
+    [SerializeField] GameObject UpperPart;
+    [SerializeField] float delayTime;
+    [SerializeField] AnimationCurve yPositionOfTime;
+
+    private void Update()
+    {
+        float y = yPositionOfTime.Evaluate(delayTime + (float)GameManager.SyncTimer);
+        UpperPart.transform.rotation = Quaternion.Euler(y, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+    }
+}
