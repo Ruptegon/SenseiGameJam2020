3beb840871e78fb4b189dcd59b079742a6fff0dc
Bartłomiej Jarosz <rivfox.jb@gmail.com>
2020-01-26 00:35:35 +0100

    Update

---
+++ Assets/Scripts/ServerUIController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerUIController : MonoBehaviour
+{
+    private void Awake()
+    {
+        gameObject.SetActive(Net.IsServer);
+    }
+}
