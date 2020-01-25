5afa6f3035d2f71cb03b8b51d4ef5c836d8d9ef7
Derko666 <malowic@gmail.com>
2020-01-25 04:11:24 +0100

    Updated move spikes class

--- Assets/Scripts/MoveSpikes.cs
+++ Assets/Scripts/MoveSpikes.cs
@@ -6,22 +6,30 @@ public class MoveSpikes : MonoBehaviour
 {
 
     public GameObject spikes;
-    public float time;
+    public float delayTime;
+    private bool isUp;
+    public float height;
+    public float timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpikesMoving", 2.0f, 0.1f);
+        isUp = false;
+        InvokeRepeating("SpikesMoving", delayTime, timer);
     }
 
     void SpikesMoving()
     {
-        iTween.MoveBy(spikes, new Vector3(0, 5, 0), 1);
+        if(!isUp)
+        {
+            iTween.MoveBy(spikes, new Vector3(0, height, 0), 1);
+            isUp = true;
+        }
+        else if (isUp)
+        {
+            iTween.MoveBy(spikes, new Vector3(0, -height, 0), 1);
+            isUp = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
