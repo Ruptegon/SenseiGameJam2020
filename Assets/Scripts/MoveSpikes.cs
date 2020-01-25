720f17afe1fedd2fb3f4e34a90cc62accbb996b9
Derko666 <malowic@gmail.com>
2020-01-25 04:04:22 +0100

    Added ITween and fixed Spike prefab

---
+++ Assets/Scripts/MoveSpikes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSpikes : MonoBehaviour
+{
+
+    public GameObject spikes;
+    public float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpikesMoving", 2.0f, 0.1f);
+    }
+
+    void SpikesMoving()
+    {
+        iTween.MoveBy(spikes, new Vector3(0, 5, 0), 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
