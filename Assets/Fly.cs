0a442b17ffb1f2fd1e00fda8d1148d0d08adb8b0
rivFox <rivfox.jb@gmail.com>
2020-01-26 12:33:11 +0100

    fix

--- Assets/Fly.cs
+++ Assets/Fly.cs
@@ -10,7 +10,11 @@ public class Fly : MonoBehaviour
 
     private void Start()
     {
-        direction = GetComponentInParent<ArrowSpit>().Direction;
+        var arrowSpit = GetComponentInParent<ArrowSpit>();
+        if (arrowSpit) 
+        {
+            direction = arrowSpit.Direction;
+        }
 
         switch (direction)
         {
