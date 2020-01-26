e9a82421494fba2848ccfcb1c647f13035072ef1
rivFox <rivfox.jb@gmail.com>
2020-01-26 04:59:07 +0100

    DungeonKeeper by Raycast

--- Assets/Scripts/DungeonKeeper.cs
+++ Assets/Scripts/DungeonKeeper.cs
@@ -7,12 +7,14 @@ public class DungeonKeeper : MonoBehaviour
 {
     private GameObject toSpawn;
     private Vector3 spawnLocation;
+    private Plane plane;
 
     [SerializeField] private Button batButton;
 
     private void Start()
     {
         batButton.onClick.AddListener(ButtonBat);
+        plane = new Plane(Vector3.up, Camera.main.transform.position.y);
     }
 
     // Update is called once per frame
@@ -20,32 +22,25 @@ public class DungeonKeeper : MonoBehaviour
     {
         if(Input.GetMouseButton(0))
         {
-            //Debug.Log("Mouse down 2!");
+            Debug.Log("Mouse down 2!");
+
             if (toSpawn != null)
             {
-                var camera = Camera.main;
-                var mousePos = Input.mousePosition;
-                mousePos.z = camera.transform.position.z;
-                Vector3 worldPos = camera.ScreenToWorldPoint(mousePos);
-                //Debug.Log("Mouse pos = " + mousePos);
-                //Debug.Log("World pos = " + worldPos);
-                worldPos += new Vector3(0.5f, 0.5f, 0.5f);
-                if (worldPos.x >= 0 && worldPos.z >= 0 && worldPos.x <= 11 && worldPos.z <= 40)
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                float enter = 0.0f;
+
+                if (plane.Raycast(ray, out enter))
                 {
-                    GameManager.Builder.BuildObject(toSpawn, (int)worldPos.x, (int)worldPos.z);
+                    Vector3 worldPos = ray.GetPoint(enter);
+                    worldPos += new Vector3(0.5f, 0.5f, 0.5f);
+                    if (worldPos.x >= 0 && worldPos.z >= 0 && worldPos.x < 7 && worldPos.z < 30)
+                    {
+                        GameManager.Builder.BuildObject(toSpawn, (int)worldPos.x, (int)worldPos.z);
+                        Debug.Log("Mouse pos = " + Input.mousePosition);
+                        Debug.Log("World pos = " + worldPos);
+                    }
                 }
             }
-        }
-    }
-
-    private void OnMouseDown()
-    {
-        //Debug.Log("Mouse down!");
-        var mousePos = Input.mousePosition;
-        Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
-        if (worldPos.x >= 0 && worldPos.z >= 0 && worldPos.x <= 11 && worldPos.z < 50) 
-        {
-            GameManager.Builder.BuildObject(toSpawn, (int)worldPos.x, (int)worldPos.z);
         }
     }
 
