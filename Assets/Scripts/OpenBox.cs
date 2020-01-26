1e4d83362ad4a2e9ae046097a1bbf87da3a6415e
Bartłomiej Jarosz <rivfox.jb@gmail.com>
2020-01-26 11:13:02 +0100

    Add chests, mimin and normal

--- Assets/Scripts/OpenBox.cs
+++ Assets/Scripts/OpenBox.cs
@@ -5,12 +5,41 @@ using UnityEngine;
 public class OpenBox : MonoBehaviour
 {
     [SerializeField] GameObject UpperPart;
-    [SerializeField] float delayTime;
     [SerializeField] AnimationCurve yPositionOfTime;
+
+    public enum ChestType { none, Chest, Mimic };
+    [SerializeField] ChestType type = ChestType.none;
+    [SerializeField] int scoreToAdd = 16;
+
+    bool goOpen = false;
+    float startOpenTime;
 
     private void Update()
     {
-        float y = yPositionOfTime.Evaluate(delayTime + (float)GameManager.SyncTimer);
-        UpperPart.transform.rotation = Quaternion.Euler(y, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        if (goOpen)
+        {
+            float y = yPositionOfTime.Evaluate(startOpenTime + GameManager.SyncTimer);
+            UpperPart.transform.rotation = Quaternion.Euler(y, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<Player>();
+        if (!player || !player.isLocalPlayer)
+            return;
+
+        startOpenTime = GameManager.SyncTimer;
+        goOpen = true;
+
+        if (type == ChestType.Chest)
+        {
+            player.Score += scoreToAdd;
+            scoreToAdd = 0;
+        }
+
+        if (type == ChestType.Mimic)
+            player.Damage();
     }
 }
