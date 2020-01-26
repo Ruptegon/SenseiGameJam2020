023627972b400f8c8562b5fdc11b7708e760d558
rivFox <rivfox.jb@gmail.com>
2020-01-26 11:27:14 +0100

    4 arrows?

--- Assets/Fly.cs
+++ Assets/Fly.cs
@@ -6,6 +6,7 @@ public class Fly : MonoBehaviour
 {
     private float lifeTime = 30f;
     private float speed = 1f;
+    [SerializeField] int direction = 0; // 0 - forward, 1 - backwards, 2 - left, 3 - right
 
     void Update()
     {
@@ -13,6 +14,21 @@ public class Fly : MonoBehaviour
         if (lifeTime <= 0f)
             Destroy(gameObject);
 
-        transform.position += transform.forward * speed * Time.deltaTime;
+        switch (direction) 
+        {
+            case 0:
+                transform.position += transform.forward * speed * Time.deltaTime;
+                break;
+            case 1:
+                transform.position += transform.forward * -1 * speed * Time.deltaTime;
+                break;
+            case 2:
+                transform.position += new Vector3(transform.forward.z, transform.forward.y, transform.forward.x) * speed * Time.deltaTime;
+                break;
+            case 3:
+                transform.position += new Vector3(transform.forward.z, transform.forward.y, transform.forward.x) * -1 * speed * Time.deltaTime;
+                break;
+        }
+        
     }
 }
