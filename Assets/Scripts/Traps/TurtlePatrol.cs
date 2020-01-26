0fd139041b113e76d37a34682d2b953f7d5c67c9
Bartłomiej Jarosz <rivfox.jb@gmail.com>
2020-01-26 11:45:51 +0100

    refactor TurtlePatrol.cs

--- Assets/Scripts/Traps/TurtlePatrol.cs
+++ Assets/Scripts/Traps/TurtlePatrol.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class TurtlePatrol : MonoBehaviour
 {
-    [SerializeField] AnimationCurve positionYCurve; //jumps!
-    [SerializeField] AnimationCurve rotation; //0 - left or 1 - right
-    float lastTime = 0;
+    [SerializeField] AnimationCurve positionX;
+    [SerializeField] AnimationCurve positionZ;
+    [SerializeField] AnimationCurve positionY;
+    [SerializeField] AnimationCurve rotationY;
+
+    Vector3 startPosition;
 
     private void Start()
     {
-        rotation.postWrapMode = WrapMode.Loop;
-        positionYCurve.postWrapMode = WrapMode.Loop;
+        startPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position += rotation.Evaluate((float)GameManager.SyncTimer) * transform.forward * ((float)GameManager.SyncTimer - lastTime);
-        transform.position = new Vector3(transform.position.x, positionYCurve.Evaluate((float)GameManager.SyncTimer), transform.position.z);
-        lastTime = (float)GameManager.SyncTimer;
+        var time = GameManager.SyncTimer;
+        transform.rotation = Quaternion.Euler(0f, rotationY.Evaluate(time), 0f);
+        transform.position = new Vector3(positionX.Evaluate(time), positionY.Evaluate(time), positionZ.Evaluate(time));
     }
 }
